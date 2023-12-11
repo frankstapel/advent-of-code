9066bc5d902ae76d9c9169ee83311ff84c45e206
Frank Stapel <frank.stapel@mosadex-ehealth.nl>
2023-12-11 19:15:47 +0100

    Day 11 part 2

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -37,18 +37,76 @@ def a(content: [str]) -> None:
             if map[x][y] == '#':
                 nodes.append((x, y))
 
-    total_distance = 0
-
     # For each node, loop over the remaining nodes and calculate the distance
+    total_distance = 0
     for a in range(len(nodes) - 1):
         for b in range(a + 1, len(nodes)):
             total_distance += calculate_distance(nodes[a], nodes[b])
-
     print(total_distance)
 
 
+def calculate_galactic_distance(map, a: int, b: int, galaxy_size=1000000) -> int:
+    distance = 0
+    a, b = (min(a[0], b[0]), min(a[1], b[1])
+            ), (max(a[0], b[0]), max(a[1], b[1]))
+
+    # Start with a, follow the path, add 1 for not X, galaxy_size for X
+    for x in range(1, b[0] - a[0]):
+        if map[a[0] + x][a[1]] == 'X':
+            distance += galaxy_size
+        else:
+            distance += 1
+    for y in range(1, b[1] - a[1]):
+        if map[a[0]][a[1] + y] == 'X':
+            distance += galaxy_size
+        else:
+            distance += 1
+
+    # Include corners
+    if not (a[0] == b[0] or a[1] == b[1]):
+        distance += 1
+
+    # Include final node
+    distance += 1
+    return distance
+
+
 def b(content: [str]) -> None:
-    print(content)
+    # Expand the map vertically
+    expanded_map = []
+    for line in content:
+        line = list(line)
+        if '#' in line:
+            expanded_map.append(line)
+        else:
+            # Expand the galaxy!
+            expanded_map.append(['X' for _ in range(len(line))])
+
+    # Expand the map horizontally
+    map = np.array(expanded_map).T
+    expanded_map = []
+    for line in map:
+        if '#' in line:
+            expanded_map.append(line)
+        else:
+            # Expand the galaxy!
+            expanded_map.append(['X' for _ in range(len(line))])
+    map = np.array(expanded_map)
+
+    # Loop over all locations to find # coordinates
+    nodes = []
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            if map[x][y] == '#':
+                nodes.append((x, y))
+
+    # For each node, loop over the remaining nodes and calculate the distance
+    total_distance = 0
+    for a in range(len(nodes) - 1):
+        for b in range(a + 1, len(nodes)):
+            total_distance += calculate_galactic_distance(
+                map, nodes[a], nodes[b])
+    print(total_distance)
 
 
 ############################
